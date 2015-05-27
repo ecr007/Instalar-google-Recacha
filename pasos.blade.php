853bf024ee8532256dc9da7c856b2ac25cc2db10
Ever <evercuevas1000@gmail.com>
2015-05-27 09:03:53 -0400

    Create pasos.blade.php

---
+++ pasos.blade.php
@@ -0,0 +1,29 @@
+<?php 
+  /**
+   * Codigo del receptor
+   */
+  
+  //Incluimos la clase
+  require 'recaptchalib.php';
+  
+  // tu clave secreta
+  $secret = "6LepjgITAAAAALaJvs7pV3OpHTWkr-k0MOrv4qMj";
+
+  // respuesta vacía
+  $response = null;
+		 
+  // comprueba la clave secreta
+  $reCaptcha = new ReCaptcha($secret);
+  
+  //Accion a realizar en caso de no ser valido el capcha
+  if ($response == null && $response->success == false) {
+  	die(header("Location: ".$me_voy."=3"));
+  }
+?>
+
+{{--Emisor--}}
+
+<script src='https://www.google.com/recaptcha/api.js?hl=es'></script>
+
+{{--Donde queremos que aperezca el captcha--}}
+<div class="g-recaptcha" data-sitekey="CLAVE SECRETA"></div>
